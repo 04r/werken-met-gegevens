b3b469097241abc3e8070accb609235e1a61a9e1
Seyit a <seyitaliaydin04@gmail.com>
2021-09-06 11:57:39 +0200

    first commit

---
+++ calculations/calc.py
@@ -0,0 +1,39 @@
+croissant = 0.39
+
+stokbrood = 2.78
+
+kortingsbon = 0.50
+
+print(f"ik wil 17 croissantjes die {croissant} per stuk kost, ook wil ik 2 stokbroden die {stokbrood} per stuk kosten.\nEn, ik heb 3 kortingsbonnen ter waarde van {kortingsbon}.")
+
+a = croissant * 17
+b = stokbrood * 2
+c = 3 * kortingsbon
+
+print("Ik moet betalen:")
+print(a + b - c)
+print("\n\n")
+print("Tweede opdracht\n\n")
+
+
+
+
+# Tweede opdr
+
+ticket = 7.45
+
+vrienden = 3 
+
+jij = 1 
+
+iedereen = vrienden + jij
+
+vr_gameseat = 0.37 #Per 5 min
+
+totaal = iedereen * ticket
+
+minuten = vr_gameseat * 9
+
+alles = totaal + minuten
+
+print(f"ik moet {alles} betalen")
